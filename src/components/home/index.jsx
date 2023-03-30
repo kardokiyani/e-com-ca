12724c2ca128a60648de4d756ab4644489f4d07a
kardokiyani <91535515+kardokiyani@users.noreply.github.com>
2023-03-30 18:14:32 +0200

    Discount price added
    Added the discount price to the products which have a discount price. Then styled it.

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-
 import { Link } from "react-router-dom";
 
 const url = "https://api.noroff.dev/api/v1/online-shop";
@@ -36,9 +35,23 @@ export function ProductPage() {
     return <div>Error loading data</div>;
   }
 
-  const filteredProducts = products.filter((product) =>
-    product.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const filteredProducts = products
+    .filter((product) =>
+      product.title.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    .map((product) => {
+      const discountPercentage =
+        product.discountedPrice < product.price
+          ? Math.round(
+              ((product.price - product.discountedPrice) / product.price) * 100
+            )
+          : null;
+
+      return {
+        ...product,
+        discountPercentage,
+      };
+    });
 
   return (
     <div className="productPageContainer">
@@ -57,7 +70,14 @@ export function ProductPage() {
             <h2>{product.title}</h2>
             <img src={product.imageUrl} alt={product.title} />
             <p className="descriptionStyle">{product.description}</p>
-            <p className="priceStyle">{product.price}</p>
+            {product.discountPercentage && (
+              <p className="discountedPriceStyle">
+                {product.discountedPrice} ({product.discountPercentage}% off)
+              </p>
+            )}
+            {!product.discountPercentage && (
+              <p className="priceStyle">{product.price}</p>
+            )}
             <Link to={`/product/${product.id}`} className="viewProductButton">
               View product
             </Link>
